886cbc6646e008d639762b212cbb2bc8cb1a3fff
Sandi Letić <sandiletic@gmail.com>
2017-11-22 11:13:16 +0100

    rework

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # SensTriggerAudio
-A bash script that triggers audio file playback depending on arduino input
+A bash script that triggers audio file playback on a raspberry depending on sensor input fed via an arduino 
+
+Add the following entry to /etc/rc.local for automatic start
+`/home/pi/.sh &`
--- SensTrigger.sh
+++ SensTrigger.sh
@@ -1,62 +1,32 @@
 #!/bin/bash
-#
-#skripta za pustanje audio fileova na maketi
-#
-#dodati entry u /etc/rc.local:
-#
-# /home/pi/muzej.sh &
-#
 
-sleep 5
-
-tmux
-
-stty -F /dev/ttyACM0 115200
-
-cd /home/pi
-
-while true
-
-	do
-
-		cat /dev/ttyACM0 | head -n 1 | while read LINE
-
-		do
-
-			case "$LINE" in
-
-				"1")  echo "1"
-					mpg123 Stone_kicking.mp3
-					;;
-
-				"2")  echo  "2"
-					mpg123 Stone_wood.mp3
-					;;
-
-				"3")  echo  "3"
-					mpg123 Tree-falling.mp3
-					;;
-
-				"4") echo  "4"
-					mpg123 Stone_fine.mp3
-					;;
-
-				"5")  echo "5"
-					mpg123 Sheep.mp3
-					;;
-
-				"6")  echo "6"
-					mpg123 Stone_grinding.mp3
-					;;
-
-			esac
-
-			LINE=""
-
-			sleep 1
-
-		done
-
+sleep 5 
+stty -F /dev/ttyACM0 115200 #change the baud rate 
+cd /home/pi 
+while true do
+	cat /dev/ttyACM0 | head -n 1 | while read LINE do
+		case "$LINE" in
+			"1") echo "1"
+				mpg123 Stone_kicking.mp3
+			;;
+			"2") echo "2"
+				mpg123 Stone_wood.mp3
+			;;
+			"3") echo "3"
+				mpg123 Tree-falling.mp3
+			;;
+			"4") echo  "4"
+				mpg123 Stone_fine.mp3
+			;;
+			"5") echo "5"
+				mpg123 Sheep.mp3
+			;;
+			"6") echo "6"
+				mpg123 Stone_grinding.mp3
+			;;
+		esac
+		LINE=""
+		sleep 1
 	done
-
+done
 exit 0
